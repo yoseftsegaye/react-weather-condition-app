c2e98625e971bdc14636c663926f2c6567b889d6
yoseftsegaye <yoseftsegaye78@gmail.com>
2024-10-17 23:57:48 -0700

    i added ENTER key handler and make input tag full screen for small screen

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -30,11 +30,16 @@ function SearchInput({ setCity, units, setUnits }) {
     }
 
     return (
-        <div className='ms-2 flex flex-row justify-center my-3'>
+        <div className='ms-2 flex flex-col sm:flex-row justify-center my-3'>
             <div className="flex flex-row w-[100%] items-center justify-center space-x-4">
                 <input
                     value={query}
                     onChange={(e) => setQuery(e.currentTarget.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            handleButton();
+                        }
+                    }}
                     type="text"
                     placeholder='search for city...'
                     className='text-[100%] font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase'
@@ -51,7 +56,7 @@ function SearchInput({ setCity, units, setUnits }) {
                 />
             </div>
 
-            <div className='ms-1 flex flex-row w-1/2 items-center justify-center'>
+            <div className='ms-1 flex flex-row w-full sm:w-1/2 items-center justify-center'>
                 <button
                     name='metric'
                     className='text-xl text-white font-light transition ease-out hover:scale-125'
